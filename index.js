d43892be5db9e7cacb27a6815e11bc6af6a3465e
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-21 14:40:48 +0200

    complete 3.3 and fix variable names

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-let notes = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -33,16 +33,22 @@ app.use((request, response, next) => {
     next()
 })
 
-app.get('/', (request, response) => {
-    response.send('<h2>Phonebook<h2>')
-})
+app.get('/', (request, response) => {response.send('<h2>Phonebook<h2>')})
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${notes.length} people</p><p>${request.requestTime}</p>`)
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${request.requestTime}</p>`)
 })
 
-app.get('/api/persons', (request, response) => {
-    response.json(notes)
+app.get('/api/persons', (request, response) => {response.json(persons)})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
