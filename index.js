7f2f598ec7f7c4b8257e00b07f255b26a4b1b463
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-21 15:40:32 +0200

    complete 3.8

--- index.js
+++ index.js
@@ -39,8 +39,9 @@ app.use((request, response, next) => {
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
-
+morgan.token('body', (request, response) => {return JSON.stringify(request.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+// This is equivalent to 'tiny' + body (from the docs)
 
 app.get('/', (request, response) => {response.send('<h2>Phonebook<h2>')})
 
